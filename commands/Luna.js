d36885da9d43352342cd9bf0fc4d53790ba104ab
John Doe <aali.charania@gmail.com>
2021-09-17 19:56:18 -0400

    hi

--- commands/Luna.js
+++ commands/Luna.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "Luna is washed",
     execute(message, args){
         message.channel.send("https://media.discordapp.net/attachments/231417330083692544/888517557265522728/unknown-10.png");
-        message.channel.sent("https://cdn.discordapp.com/attachments/231417330083692544/888528228992557126/unknown.png")
+        message.channel.send("https://cdn.discordapp.com/attachments/231417330083692544/888528228992557126/unknown.png")
     }
 }
