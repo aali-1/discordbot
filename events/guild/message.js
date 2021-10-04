3cd28d763b270a06f9566e12bc5052dbdf99992c
John Doe <aali.charania@gmail.com>
2021-10-04 18:14:13 -0400

    ratio

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,4 +1,5 @@
 module.exports = (Discord, client, message)=>{
+    if(message.content==='ratio' && message.author.id==='338853337678086146' || message.author.id==='192714995946094593') message.react('⬆');
     const prefix = '!';
     const prefix2 = '~';
     if(!message.content.startsWith(prefix) && !message.content.startsWith(prefix2) || message.author.bot) return;
