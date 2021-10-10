f2a6ba7650273c895019b0c10e8ac8e50d9f261d
John Doe <aali.charania@gmail.com>
2021-10-09 20:04:14 -0400

    new music

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({partials:['MESSAGE']});
 let port = process.env.PORT;
 
 const distube = require('distube');
-client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true, youtubeCookie: '__Secure-3PSID=BwhgkAsDP-wTiFKfz6CURJhcJzZigL4oFEUzp0AN3Nem9iO5DiFgu2_3L7dnwHoqiX_9ig.; __Secure-3PAPISID=Q-rvtOqhjX3ub8BV/AnYmj2CedsM1H4IhN; GPS=1; VISITOR_INFO1_LIVE=AsfpqLRc_U4; YSC=vCcgzerhboU; PREF=tz=America.New_York&f6=40000000' });
+client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true,});
 
 global.deleted = undefined;
 global.edited = undefined;
