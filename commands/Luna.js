9579a92ff4839f2e3849e2d753929669aa4a5a46
John Doe <aali.charania@gmail.com>
2022-03-18 13:59:57 -0400

    yoom

--- commands/Luna.js
+++ commands/Luna.js
@@ -1,8 +1,10 @@
 module.exports = {
     name: 'luna',
-    aliases: 'bisharp',
+    aliases: ['bisharp','yoom'],
     description: "Luna is washed",
     execute(client, message, args, Discord, cmd){
+        if(cmd==="yoom")
+            message.reply({content:"https://cdn.discordapp.com/attachments/642894213062590476/954438207909552148/606acf82e082331f957ffe9adf90fbd2.png",allowedMentions: { repliedUser: false }})
         if(cmd==="luna")
             message.reply({content:"https://media.discordapp.net/attachments/231417330083692544/888517557265522728/unknown-10.png",allowedMentions: { repliedUser: false }});
         if(cmd==="bisharp")
