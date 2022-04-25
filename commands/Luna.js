d8e20b3b066b4b8a5d1fa2df9337bfe8d610f6ab
John Doe <aali.charania@gmail.com>
2022-04-24 23:32:12 -0400

    fade

--- commands/Luna.js
+++ commands/Luna.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'luna',
-    aliases: ['bisharp','yoom'],
+    aliases: ['bisharp','yoom','fade'],
     description: "Luna is washed",
     execute(client, message, args, Discord, cmd){
         if(cmd==="yoom")
@@ -9,5 +9,7 @@ module.exports = {
             message.reply({content:"https://media.discordapp.net/attachments/231417330083692544/888517557265522728/unknown-10.png",allowedMentions: { repliedUser: false }});
         if(cmd==="bisharp")
             message.reply({content:'https://cdn.discordapp.com/attachments/827067376301244416/827256518386384936/bisharpmoment.gif',allowedMentions: { repliedUser: false }});
+        if(cmd=="fade")
+            message.reply({content:'https://i.gyazo.com/59b2ab61144798f69e7eb79caece275f.png',allowedMentions:{repliedUser:false}})
     }
 }
