a815697483802d5807b9464c1e198264a42c98e4
AaliCharania <aali.charania@gmail.com>
2022-09-25 20:46:18 -0400

    config change

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
 //const config = require('./config.json');
 const client = new Discord.Client({partials:['Partials.Channel'],intents: [
     "Guilds", 
